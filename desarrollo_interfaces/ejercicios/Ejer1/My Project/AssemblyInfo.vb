0ccea80a967d979406934783bbaa13294403199e
José Luis García Hernández <n222081@rtrtr.com>
2012-10-02 09:07:37 +0200

    primer programa VB

---
+++ desarrollo_interfaces/ejercicios/Ejer1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Ejer1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Ejer1")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("769461b3-9957-47e6-8631-9423f75f03d5")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de versión de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
